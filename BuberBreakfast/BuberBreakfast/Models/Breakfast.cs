38092523469029ca61e6585ba0ec7bcb976c10c1
Louai-Alaranji <alaranjileo99@gmail.com>
2023-08-22 08:23:07 +0200

    Created Models
    created the Models folder and changed a bit in the controller.

--- BuberBreakfast/BuberBreakfast/Models/Breakfast.cs
+++ BuberBreakfast/BuberBreakfast/Models/Breakfast.cs
@@ -11,4 +11,26 @@ public class Breakfast{
     public DateTime LastModifiedDateTime { get; }
     public List<string> Savory { get; }
     public List<string> Sweet { get; }
+
+//below is the constructor
+    private Breakfast(
+        Guid id,
+        string name,
+        string description,
+        DateTime startDateTime,
+        DateTime endDateTime,
+        DateTime lastModifiedDateTime,
+        List<string> savory,
+        List<string> sweet)
+    {
+        // Here we can inforce for ex, name will have 3 char max. 
+        Id = id;
+        Name = name;
+        Description = description;
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime;
+        LastModifiedDateTime = lastModifiedDateTime;
+        Savory = savory;
+        Sweet = sweet;
     }
+}
