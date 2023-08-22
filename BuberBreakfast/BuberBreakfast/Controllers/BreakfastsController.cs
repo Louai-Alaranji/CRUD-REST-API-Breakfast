cc280898ea5afc561241ae2cc47f78302d772725
Louai-Alaranji <alaranjileo99@gmail.com>
2023-08-22 07:27:26 +0200

    Controller Class Test CRUD written now
    wrote the CRUD operations without total functionality yet for testing.
    run:
    dotnet run --project .\BuberBreakfast\

--- BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
@@ -9,4 +9,19 @@ public class BreakfastsController : ControllerBase {
     public IActionResult CreateBreakfast(CreateBreakfastRequest request){
         return Ok(request);
     }
+
+    [HttpGet("/breakfast/{id:guid}")]
+    public IActionResult GetBreakfast(Guid id){
+        return Ok(id);
+    }
+
+    [HttpPut("/breakfast/{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request){
+        return Ok(request);
+    }
+
+    [HttpDelete("/breakfast/{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id){
+        return Ok(id);
+    }
 }
